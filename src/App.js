ffec234cccc029b3150dfb798f1f62deed78758d
Vikranth Jagdish <83574680+sinkingcircle@users.noreply.github.com>
2024-10-03 15:45:24 +0000

    fized issues

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
-  );
-}
-
-function Square({ value }) {
-  function handleclick() {
-    console.log("clicked");
-  }
-  return (
-    <button className="square" onclick={handleclick}>
-      {" "}
-      {value}{" "}
-    </button>
   );
 }
